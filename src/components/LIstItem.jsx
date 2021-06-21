d55255183701b6f09e7d3cfc2580f305ddfacc6d
violetapugliese <violeta.pugliese@gmail.com>
2021-06-21 19:35:27 -0300

    deploy 3

--- src/components/LIstItem.jsx
+++ src/components/LIstItem.jsx
@@ -5,9 +5,9 @@ import playIcon from "../assets/static/playIcon.png";
 const ListItem = ({ id, music, name, image}) => {
   return (
     <div className="group flex flew-wrap w-5/6 items-center justify-between h-auto bg-white  shadow-lg rounded-full m-2 p-1 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 ">
-      <figure className="inline-block w-14 h-14 photoWidth rounded-full bg-gray-800">
+      <div className="inline-block w-14 h-14 photoWidth rounded-full bg-gray-800">
         <img src={`${image}`} alt={`${name}`} />
-      </figure>
+      </div>
       <p className="mx-2 text-center">{name}</p>
       <button
         id="playButton"
