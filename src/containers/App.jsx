ee6a204119448c79ebd218e64aa4d183b999364a
violetapugliese <violeta.pugliese@gmail.com>
2021-06-21 19:30:48 -0300

    deploy 2

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,18 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link,
-  NavLink,
+  Link
 } from "react-router-dom";
 
 import Header from "../components/Header";
 import Serch from "../components/Serch";
 import Main from "../components/Main";
 import Video from "../components/Video";
-import VideoPlayer from "../components/VideoPlayer";
-import VideoData from "../components/VideoData";
 import List from "../components/List";
 import ListItem from "../components/ListItem";
 import Footer from "../components/Footer";
@@ -20,11 +17,9 @@ import useInitialState from "../hooks/useInitialState";
 
 import "../styles.css";
 
-// const API = "http://localhost:3000/initialState";
 const API = "https://violetapugliese.github.io/mdrAPIRest/db.json";
 
 const App = () => {
-  // const [video, setVideo] = useState([]);
 
   const data = useInitialState(API);
 
@@ -53,8 +48,7 @@ const App = () => {
                   <Link
                     to={`${item.name}`}
                     key={item.id}
-                    className="w-full flex items-center justify-center "
-                  >
+                    className="w-full flex items-center justify-center ">
                     <ListItem key={item.id} {...item} />
                   </Link>
                 ))}
