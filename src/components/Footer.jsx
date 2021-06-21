d92663754188c05003f517bc5b4c097e4f4f99f8
violetapugliese <violeta.pugliese@gmail.com>
2021-06-21 19:09:41 -0300

    config firebase

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 const Footer = () => (
-<<<<<<< HEAD
     <footer className="absolute b-0 w-full h-10 bg-black">
-=======
-    <footer className="absolute b-0 w-full h-10 bg-black lg:bg-transparent">
->>>>>>> gh-pages
     </footer>
 );
 
