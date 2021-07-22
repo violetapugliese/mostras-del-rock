6bcd798403ad47dbbe6889f0aae8a08068b9e8be
violetapugliese <violeta.pugliese@gmail.com>
2021-07-21 22:49:36 -0300

    search

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => (
     <footer className="absolute b-0 w-full h-10 bg-black text-white text-center">
-      <p className="my-4 text-xs"><a href="https://github.com/violetapugliese">Violeta Pugliese © </a> - 2021</p> 
+      <p className="my-4 text-xs"><a href="https://github.com/violetapugliese"> Violeta Pugliese © </a> - 2021</p> 
     </footer>
 );
 
